7622035acc6634d5d1ae4ec821ed5b8c683314ac
Charnaye Grier <charnayegrier@gmail.com>
2022-08-22 12:33:39 -0400

    studying done with official intro to big O

--- Examples.js
+++ Examples.js
@@ -32,3 +32,16 @@ console.log(`Time elapsed: ${(t2 -t1) / 1000} seconds`)
 // there's three operations in the function addUp. 1 multiplication, 1 addition, and 1 division. so 3 simple operations, regardless of the size of n
 
 //in the function addUpTo, theres a for loop. and so the amount of operations depends on n in this case. its doing n assignments, n comparisons, and n additions all over the place.
+
+// for the function addUp, it has a big O of 1, O(1) meaning as n grows, it doesn't really reflect in the runtime. it always has 3 operations.
+
+// for the function addUpTo, it has a big O of n, O(n) meaning as n (input) grows the runtime grows. the number of operations is (eventually) bounded by a multiple of n
+
+function printAllPairs(n) {
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            console.log(i, j)
+        }
+    }
+}
+// this function includes a nested loop, where one for loop is inside of the other. the big O notation here is: O of n squared, O(n²)
