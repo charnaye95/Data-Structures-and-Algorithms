d69d27dfe336a5b485aee79e21322f3f8407a741
Charnaye Grier <charnayegrier@gmail.com>
2022-08-31 15:42:07 -0400

    went over intro to divide and conquer pattern

--- Examples.js
+++ Examples.js
@@ -295,3 +295,25 @@ const maxSubarraySum = (array, num) => {
 }
 
 maxSubarraySum([5,2,8,9,3,1,3,5,6,10],4)
+
+// Divide and Conquer Pattern
+
+// Example: Given a sorted array of integers, write a function called search, that accepts a value and returns the index where the value passed to the function is located. If the value is not found, return -1
+
+const search = (array, value) => {
+    let min = 0
+    let max = array.length - 1
+    while (min <= max) {
+        let middle = Math.floor((min + max) / 2)
+        let currentElement = array[middle]
+
+        if (array[middle] < value) {
+            min = middle + 1
+        } else if (array[middle] > value) {
+            max = middle - 1
+        } else {
+            return middle
+        }
+    }
+    return -1
+}
